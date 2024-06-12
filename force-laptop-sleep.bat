ab5549046a3201139d5fdf3988b919e2dac4b89b
mgallivanoni <sigmud@gmail.com>
2024-06-12 14:25:45 +0200

    start and end times are now parameters

--- force-laptop-sleep.bat
+++ force-laptop-sleep.bat
@@ -1,13 +1,15 @@
 @echo off
 
-::REM entering batch folder
+::REM entering the folder where this batch script is located
 CD /d C:\tmp\force-win-sleep\
 
 ::REM change thes evariables to fit your scenario
 set THE_LOG=".\00--force_sleep_log.txt"
+set START_WTIME=0830
+set END_WTIME=1810
 
 ::REM local file to hinibit sleep
-set USER_SAID_NO=".\no-thanks" >NUL
+set USER_SAID_NO=".\no-thanks"
 set office_network_prefix=191.168.111
 set home_network_prefix=191.168.222
 
@@ -23,7 +25,7 @@ ipconfig /all | find "IPv4" | find "%home_network_prefix%" >> zl.txt
 SLEEP 10
 ::REM the sleep command above may help mocking zl.txt when testing this batch script
 
-type zl.txt | wc -l > l.txt
+type zl.txt | find /c "." > l.txt
 
 for /f "delims=," %%i in ('type l.txt') DO set laptop_in_office=%%i
 
@@ -32,9 +34,7 @@ echo "laptop_in_office is <<%laptop_in_office%>>"  >>%THE_LOG%
 if %laptop_in_office% gtr 0 goto the_beginning
 
 ::REM NOW CHECK IF WE ARE OUTSIDE WORKING HOURS
-::REM python -c "import datetime ; print(datetime.datetime.now().strftime('%%H%%M'))" > l.txt
 for /f "delims=: tokens=1,2" %%i in ('echo %time%') DO echo %%i%%j > l.txt
-
 
 sleep 10
 ::REM NB: the sleep command above may help mocking l.txt when testing this batch script
@@ -43,8 +43,8 @@ for /f "delims=," %%i in ('type l.txt') DO set ttime=%%i
 
 echo "current time is <<%ttime%>>"  >>%THE_LOG%
 
-if %ttime% gtr 1810  goto proceed
-if 0830 gtr %ttime%  goto proceed
+if %ttime% gtr %END_WTIME%  goto proceed
+if %START_WTIME% gtr %ttime%  goto proceed
 ::REM if we reach this point it is working hours so we need to go back to the beginning and wait 
 goto the_beginning
 
@@ -62,8 +62,8 @@ if not exist "%USER_SAID_NO%" (
      powershell "[console]::beep(500,300)"
 
      echo wait  2 minutes then suspend >>%THE_LOG%
-	 ::REM 2 minutes should been enough for emergency recovery to
-	 ::REM stop suspend loops from going berserk
+     ::REM 2 minutes should been enough for emergency recovery to
+     ::REM stop suspend loops from going berserk
      sleep 120
 
      powershell "[console]::beep(500,300)"
@@ -72,7 +72,7 @@ if not exist "%USER_SAID_NO%" (
 
      echo send laptop to 'powrprof.dll,SetSuspendState 0,1,0' %time% >>%THE_LOG%
      rundll32.exe powrprof.dll,SetSuspendState 0,1,0
-	 echo the rundll command for sleep has been launched >>%THE_LOG%
+     echo the rundll command for sleep has been launched >>%THE_LOG%
 	 
 )
 
